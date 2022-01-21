c6ed25234ac32f8668904e69e77a80067d0f51f0
Carlos Vera <kikiondo117@gmail.com>
2022-01-21 17:45:17 -0600

    Feature: Add information from JSON in dynamic option

--- src/tree/index.js
+++ src/tree/index.js
@@ -1,31 +1,30 @@
-import React from 'react'
+import React from "react";
 import "./index.css";
+// * Utils
+import dataJson from "./data.json";
 
 export default function Tree() {
-  const spaceRef = React.useRef()
-  const [spaceValue, setSpaceValue] = React.useState(0)
+  const spaceRef = React.useRef();
+  const [data, setData] = React.useState(null);
 
-  React.useLayoutEffect(() => {
-    /**
-     * We need to get the value 'cause is relative by the font-size root
-     * The 5 is for the padding that the browser set by default, we could add a function
-     * to get the current padding but is for now it's not necessary
-     */
-    setSpaceValue(spaceRef.current.offsetWidth - 5)
-  }, [])
+  React.useEffect(() => {
+    setData(dataJson);
+  }, []);
+
+  if (!data) return <div>No data</div>;
 
   return (
     <div className="tree">
       <span ref={spaceRef}>&nbsp;</span>
       <p>root</p>
-      <p style={{marginLeft:`calc(${spaceValue}px * 4)`}}>ant</p>
-      <p style={{marginLeft:`calc(${spaceValue}px * 4)`}}>bear</p>
-      <p style={{marginLeft:`calc(${spaceValue}px * 8)`}}>cat</p>
-      <p style={{marginLeft:`calc(${spaceValue}px * 8)`}}>dog</p>
-      <p style={{marginLeft:`calc(${spaceValue}px * 12)`}}>
-       elephant
-      </p>
-      <p style={{marginLeft:`calc(${spaceValue}px * 4)`}}>frog</p>
+      {data.values.map((value) => {
+        const formatData = value.split(",");
+        return (
+          <p key={value} style={{ marginLeft: `calc(${formatData[1]}px * 4)` }}>
+            {formatData[0]}
+          </p>
+        );
+      })}
     </div>
   );
 }
