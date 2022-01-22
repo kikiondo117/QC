7052ea55ccb0524aac861241a826b608c273cae9
Carlos Vera <kikiondo117@gmail.com>
2022-01-21 18:03:06 -0600

    Feauture: Add new element with the selected spaces

--- src/tree/index.js
+++ src/tree/index.js
@@ -7,8 +7,20 @@ export default function Tree() {
   const spaceRef = React.useRef();
   const [data, setData] = React.useState(null);
 
+  const handleOnSubmit = (event) => {
+    event.preventDefault();
+    const spaces = event.target.elements[0].name;
+    const value = event.target.elements[0].value;
+
+    setData((prevValue) => {
+      return [...prevValue, `${value}, ${spaces}`];
+    });
+
+    event.target.reset();
+  };
+
   React.useEffect(() => {
-    setData(dataJson);
+    setData(dataJson.values);
   }, []);
 
   if (!data) return <div>No data</div>;
@@ -17,11 +29,15 @@ export default function Tree() {
     <div className="tree">
       <span ref={spaceRef}>&nbsp;</span>
       <p>root</p>
-      {data.values.reverse().map((value, key) => {
+      {data.map((value, key) => {
         const formatData = value.split(",");
         return (
           <p key={value} style={{ marginLeft: `calc(${formatData[1]}px * 4)` }}>
-            {formatData[0]} - Level {key}
+            {formatData[0]}
+            <br />
+            <form onSubmit={handleOnSubmit}>
+              <input name={formatData[1]}></input>
+            </form>
           </p>
         );
       })}
